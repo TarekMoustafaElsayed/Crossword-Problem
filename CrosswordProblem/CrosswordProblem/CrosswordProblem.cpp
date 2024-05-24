74aeb5df5cd1208009065886527c8bc16bcc81f5
TarekMoustafaElsayed <150616573+TarekMoustafaElsayed@users.noreply.github.com>
2024-05-24 20:37:01 +0300

    Testing

--- CrosswordProblem/CrosswordProblem/CrosswordProblem.cpp
+++ CrosswordProblem/CrosswordProblem/CrosswordProblem.cpp
@@ -1,9 +1,7 @@
-// CrosswordProblem.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
